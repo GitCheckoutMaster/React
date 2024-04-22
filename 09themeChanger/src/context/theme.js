f2eae72a87f4a8033fa245bc991999d0d1daedd8
MrPrince <jaymistry31170@gmail.com>
2024-04-22 17:36:10 +0530

    just checked if i was right on context API confusion in theme changer project

--- 09themeChanger/src/context/theme.js
+++ 09themeChanger/src/context/theme.js
@@ -1,8 +1,8 @@
 import { createContext, useContext } from "react";
 
 export const themeContext = createContext({
-    themeMode: "light",
-    toggleTheme: () => {}
+    // themeMode: "light",
+    // toggleTheme: () => {} // this doesn't matter because we added this as values in app.jsx and defined in app.jsx as well 
 });
 
 // this is theme provider
